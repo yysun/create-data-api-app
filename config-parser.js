aecc5188515c221b395c715437c6e02bbb9cbd4f
Yiyi Sun <yiyisun@gmail.com>
2023-09-22 13:41:18 -0400

    re-design the config

---
+++ config-parser.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+module.exports = file => {
+
+  const configFile = fs.readFileSync(file, 'utf8');
+  const config = yaml.load(configFile);
+
+  const parse_fields = fields => !Array.isArray(fields) ? [] :
+    fields.map(field => {
+      let [type, name, keys, comment] = field.split(' ');
+      if (keys && keys.startsWith('"')) {
+        comment = keys;
+        keys = undefined;
+      }
+      return { type, name, keys, comment };
+    });
+
+
+  config.databases.forEach(db => {
+
+    db.paths = [];
+
+    db.objects.forEach(obj => {
+
+      const obj_keys = Object.keys(obj);
+
+      let name, type;
+      if (obj_keys.includes('table')) {
+        name = obj.table;
+        type = 'table';
+      } else if (obj_keys.includes('query')) {
+        name = obj.query;
+        type = 'query';
+      } else if (obj_keys.includes('procedure')) {
+        name = obj.procedure;
+        type = 'procedure';
+      }
+
+      Object.keys(obj).forEach(key => {
+        if (key === 'table' || key === 'query' || key === 'procedure' || key === 'select') return;
+        let [method, path] = key.split('-');
+        const fields = parse_fields(obj[key]);
+        obj[key] = fields;
+
+        path = `${name}${path ? '/' + path : ''}`;
+        method = method.toLowerCase();
+        if (method === 'get' || method === 'delete' || method === 'patch') {
+          const keys = fields.filter(field => field.keys && field.keys.includes('PK'));
+          if (keys.length > 0) {
+            path += keys.map(key => `/:${key.name}`).join('');
+          }
+        }
+
+        const api = {
+          name,
+          type,
+          func: `${key}`,
+          path: `/${path}`,
+          method,
+          fields,
+        };
+
+        db.paths.push(api);
+      });
+    });
+
+  });
+  console.log(JSON.stringify(config, null, 2));
+  return config;
+}
--- index.js
+++ index.js
@@ -7,26 +7,17 @@ const createModel = require('./create-model');
 const createExpressApp = require('./create-express-app');
 const createAzureApp = require('./create-azure-app');
 
+const configParser = require('./config-parser');
+
 module.exports = ({ conf, cwd }) => {
 
-  const configFile = fs.readFileSync(conf, 'utf8');
-  const { name, port, path, entities, public } = yaml.load(configFile);
-  entities.forEach(entity => {
-    entity.path = `${path ?? ''}/${entity.name}`;
-    const fields = entity.fields || [];
-    entity.fields = fields.map(f => {
-      const [type, name, keys, comment] = f.split(' ');
-      const pk = keys?.indexOf('PK') > -1;
-      return { type, name, pk, comment, def: f };
-    });
-    entity.keys = entity.fields.filter(f => f.pk)
-      .map(f => f.name) || [];
-  });
 
-  fs.writeFileSync(`${cwd}/model.js`, createModel(entities));
-  fs.writeFileSync(`${cwd}/index.js`, createExpressApp(port, public, entities));
-  fs.writeFileSync(`${cwd}/app-azure.js`, createAzureApp(entities));
-  fs.writeFileSync(`${cwd}/test.http`, createTest(port, entities));
-  fs.writeFileSync(`${cwd}/README.md`, createReadme(name, port, entities));
+  const config = configParser(conf);
+
+  // fs.writeFileSync(`${cwd}/model.js`, createModel(entities));
+  // fs.writeFileSync(`${cwd}/index.js`, createExpressApp(port, public, entities));
+  // fs.writeFileSync(`${cwd}/app-azure.js`, createAzureApp(entities));
+  // fs.writeFileSync(`${cwd}/test.http`, createTest(port, entities));
+  // fs.writeFileSync(`${cwd}/README.md`, createReadme(name, port, entities));
 
 }
