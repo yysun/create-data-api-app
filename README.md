9919299586dc3f5d7fede9d19e596eeb040c37fd
Yiyi Sun <yiyisun@gmail.com>
2024-08-21 10:31:22 -0400

    add JWT refresh token

---
+++ README.md
@@ -0,0 +1,6 @@
+app.authentication = (req, res, next) => {}
+app.post('/auth/login', async (req, res) => {});
+app.post('/auth/register', async (req, res) => {});
+app.post('/auth/logout', async (req, res) => {});
+app.post('/auth/refresh', async (req, res) => {});
+
--- create-express-server.js
+++ create-express-server.js
@@ -8,21 +8,70 @@ app.use(bodyParser.json());
 ${public && `app.use(express.static('${public}'));` || ''}
 
 ${authentication === 'jwtAuth' ? `const jwt = require('jsonwebtoken');
-app.authentication = (req, res, next) => {
-  const SECRET_KEY = procees.env.SECRET_KEY;
-  const token = req.headers['authorization'];
-  if (!token) {
-    return res.status(403).send('Token not provided.');
+
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
+
+const refreshTokens = []; // In a real app, use a database
+
+app.authenticate = (req, res, next) => {
+  const accessToken = req.headers['authorization'];
+  if (!accessToken) {
+    return res.status(401).send('Access Denied. No token provided.');
   }
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(403).send('Failed to authenticate token.');
-    }
-    req.userId = decoded.userId; // Attach used id to the request object
-    req.roles = decoded.roles;  // Attach roles to the request object
-    next();
-  });
+  try {
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.sendStatus(403);
+  }
+};
+
+const createToken = (user, res) => {
+  const accessToken = jwt.sign({ user }, accessTokenSecret, { expiresIn: '1h' });
+  const refreshToken = jwt.sign({ user }, refreshTokenSecret, { expiresIn: '1d' });
+  refreshTokens.push(refreshToken); // storeRefreshToken(refreshToken);
+  res.cookie('refresh-token', refreshToken, { httpOnly: true, secure: true })
+    .send(accessToken);
 }
+app.post('/auth/register', async (req, res) => {
+}
+
+app.post('/auth/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = {}; // await verify( username, password });
+    if (!user) {
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
+    createToken(user, res);
+  } catch (error) {
+    res.status(500).json({ error: 'Login failed' });
+  }
+});
+
+app.post('/auth/refresh', (req, res) => {
+  const refreshToken = req.cookies['refresh-token'];
+  if (!refreshToken || !refreshTokens.includes(token)) {
+    return res.sendStatus(403);
+  }
+  jwt.verify(refreshToken, refreshTokenSecret, (err, user) => {
+    if (err) return res.sendStatus(403);
+    refreshTokens = refreshTokens.filter(t => t !== refreshToken); //removeRefreshToken(refreshToken);
+    createToken(user, res);
+  });
+});
+
+app.post('/auth/logout', (req, res) => {
+  const refreshToken = req.cookies['refresh-token'];
+  refreshTokens = refreshTokens.filter(t => t !== refreshToken); //removeRefreshToken(refreshToken);
+  res.sendStatus(204);
+});
 `: `
 ${authentication ? `\/\/ TODO: implement ${authentication}
 app.authentication = (_, __, next) => next();
