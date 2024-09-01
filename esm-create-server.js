30387ba8be2049fe17a8a1519ea81920d0206192
Yiyi Sun <yiyisun@gmail.com>
2024-08-31 21:03:22 -0400

    generate your own server code

--- esm-create-server.js
+++ esm-create-server.js
@@ -1,27 +1,36 @@
 
 
-module.exports = ({ port,  }) => {
+module.exports = ({ port, databases}) => {
   return `//@ts-check
+//* use the server from apprun-site, no need to load routers
 import server from 'apprun-site/server.js';
-const port = process.env.PORT || ${port};
 const app = server();
 
-// Regular logging middleware for successful requests
+//* build your own server with express
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+${databases.map(db => `// import ${db.name} from './api/${db.name}.js';`).join('\n')}
+${databases.map(db => `// app.use(${db.name});`).join('\n')}
+
 // app.use((req, res, next) => {
 //   res.on('finish', () => {
-//     req.time = new Date(Date.now()).toString();
-//     console.log('Success:', req.user, req.path, req.time);
+//     const time = new Date(Date.now()).toString();
+//     console.log('Success:', req.path, time);
 //   });
 //   next();
 // });
 
-// Error-handling middleware for logging errors
-app.use((err, req, res, next) => {
-  req.time = new Date(Date.now()).toString();
-  console.error('Error occurred:', err.message, req.user, req.path, req.time);
-  res.status(500).send('Internal Server Error');
-});
+// app.use((err, req, res, next) => {
+//   const time = new Date(Date.now()).toString();
+//   console.error('Error occurred:', req.path, time, err.message);
+//   res.status(500).send(err.message);
+// });
 
+const port = process.env.PORT || ${port};
 app.listen(port, () => console.log(\`Your app is listening on http://localhost:\${port}\`));
 `;
 }
